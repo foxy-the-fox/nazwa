ac7926e1f0a5424395113df68007feacafbd5c5c
Hanna Roch <haker6718@gmail.com>
2020-11-06 15:34:02 +0100

    initial commit

---
+++ nazwaProject/nazwaProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nazwaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
