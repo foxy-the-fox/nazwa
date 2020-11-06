f51c012741c44a79b43e798b81b24c9ffd179be6
Hanna Roch <haker6718@gmail.com>
2020-11-06 15:59:41 +0100

    switchAdded

--- nazwaProject/nazwaProject/Program.cs
+++ nazwaProject/nazwaProject/Program.cs
@@ -7,6 +7,27 @@ namespace nazwaProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int x = int.Parse(args[0]);
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("Hello world!");
+                    break;
+
+                case 2:
+                    Console.WriteLine("raspberry pi");
+                    break;
+
+                case 3:
+                    Console.WriteLine("arduino");
+                    break;
+
+                case 4:
+                    Console.WriteLine("and other");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
